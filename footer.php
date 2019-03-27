26987c6a8829825b2ef70c8a220b890434162ba5
Wayne Graham <wgraham@clir.org>
2019-03-27 09:13:13 -0700

    Update the footer information
    Update footer information for the combined CLIR/DLF job board.
    
    closes #32

--- footer.php
+++ footer.php
@@ -8,20 +8,20 @@
  * @since wpjobboard_theme 1.0
  */
 
-$is_map = basename( get_page_template() ) == "home-map.php";
-$full_width = in_array( basename( get_page_template() ), array("full-width.php", "home-map.php") );
+$is_map = basename(get_page_template()) == "home-map.php";
+$full_width = in_array(basename(get_page_template()), array("full-width.php", "home-map.php"));
 ?>
 
-<?php if(!$is_map): ?>
+<?php if (!$is_map): ?>
 </div><!-- #main .site-main -->
 </div><!-- #primary .primary -->
 <?php endif; ?>
 
-<?php if( !$full_width ): ?>
+<?php if (!$full_width): ?>
 <?php get_sidebar(); ?>
 <?php endif; ?>
 
-<?php if(!$is_map): ?>
+<?php if (!$is_map): ?>
 </div><!-- .table-row -->
 <?php endif; ?>
 <div id="footer"></div><!-- hack to fix JS -->
@@ -35,7 +35,7 @@ $full_width = in_array( basename( get_page_template() ), array("full-width.php",
   <div class="col-md-3 col-sm-6">
     <h5>Contact</h5>
     <address>
-      CLIR+DLF<br>
+      CLIR<br>
       2221 South Clark Street<br>
       Arlington, VA 22202<br>
       <abbr title="Phone">P:</abbr> (202) 939-4750<br>
@@ -46,11 +46,21 @@ $full_width = in_array( basename( get_page_template() ), array("full-width.php",
   <div class="col-md-3 col-sm-6">
     <h5>Elsewhere</h5>
       <ul class="list-unstyled">
-        <li><a href="https://twitter.com/CLIRDLF"><i class="fa fa-twitter twitter"></i> @CLIRDLF</a></li>
-        <li><a href="https://www.clir.org/"><i class="fa fa-rss rss"></i>CLIR Blog</a></li>
-        <li><a href="https://www.diglib.org/"><i class="fa fa-rss rss"></i>DLF Blog</a></li>
-        <li><a href="https://www.linkedin.com/groups/3387265/profile"><i class="fa fa-linkedin linkedin"></i> LinkedIn</a></li>
-				<li><a href="https://diglib.org/announce/"><i class="fa fa-envelope envelope"></i> DLF-Announce</a></li>
+        <li>
+          <a href="https://twitter.com/CLIRnews"><i class="fa fa-twitter twitter"></i> @CLIRnews</a> |
+          <a href="https://twitter.com/CLIRDLF"><i class="fa fa-twitter twitter"></i> @CLIRDLF</a></li>
+        <li>
+          <a href="https://www.clir.org/"><i class="fa fa-rss rss"></i>CLIR Blog</a> |
+          <a href="https://www.diglib.org/"><i class="fa fa-rss rss"></i>DLF Blog</a>
+        </li>
+        <li>LinkedIn
+          <a href="https://www.linkedin.com/company/council-on-library-and-information-resources"><i class="fa fa-linkedin linkedin"></i> CLIR</a>
+          <a href="https://www.linkedin.com/groups/3387265/profile"><i class="fa fa-linkedin linkedin"></i> DLF</a>
+        </li>
+				<li>
+          <a href="https://clir.informz.net/clir/pages/jdjb"><i class="fa fa-envelope envelope"></i> Jobs Digest mailing list</a> |
+          <a href="https://diglib.org/announce/"><i class="fa fa-envelope envelope"></i> DLF-Announce</a>
+        </li>
       </ul>
   </div>
 
@@ -63,9 +73,11 @@ $full_width = in_array( basename( get_page_template() ), array("full-width.php",
   </div>
 
   <div class="col-md-3 col-sm-6">
+    <h5>What is CLIR</h5>
+    <p>CLIR is an independent, nonprofit organization that forges strategies to enhance research, teaching, and learning environments in collaboration with libraries, cultural institutions, and communities of higher learning.</p>
     <h5>What's the DLF?</h5>
-    <p>We are networked member institutions and a robust community of practice—<em>advancing research, learning, social justice, &amp; the public good</em> through digital library technologies.</p>
-    <p><a href="https://www.diglib.org/members/join/" class="btn btn-default btn-info">Join us!</a></p>
+    <p>We are networked member institutions and a robust community of practice—<em>advancing research, learning, social justice, &amp; the public good</em> through digital library technologies. DLF is a program of CLIR.</p>
+    <p><a href="https://www.clir.org/about/become-a-sponsor-or-member" class="btn btn-default btn-info">Join us!</a></p>
   </div>
 </div>
 </div>
