05a5b7bd4fb7471826b15ead60eb94e67c5c393b
Wayne Graham <wgraham@clir.org>
2022-01-14 11:28:44 -0500

    update watching for latest version of gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,82 +1,138 @@
 const gulp = require('gulp');
-const browserSync = require('browser-sync');
-const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+// const sass = require('gulp-sass');
 const prefix = require('gulp-autoprefixer');
-const cp = require('child_process');
-const sourcemaps = require('gulp-sourcemaps');
 
-const server = browserSync.create();
+const sass = require('gulp-sass')(require('sass'));
 
 const dev_url = "localhost:8888/jobs"
 
-sass.compiler = require('node-sass');
 
-// BrowserSync
-function browser_sync(done) {
-  browserSync.init(files, {
-    server: {
-      proxy: dev_url,
-      port: 8888,
-      injectChanges: true
-    }
-  });
-  done();
+function style() {
+  return gulp.src('_sass/**/*.scss')
+  .pipe(sass({
+    // outputStyle: 'compressed',
+    outputStyle: 'expanded',
+  }).on('error', sass.logError))
+  .pipe(gulp.dest('./stylesheets'))
+  .pipe(browserSync.stream());
 }
 
-function browserSyncReload(done) {
-  browserSync.reload();
-  done();
-}
-
-
-/**
- * Browser sync for WP theme
- */
-gulp.task('browser-sync', function() {
-  var files = [
-    '**/*.php',
-    '**/*.{png,jpg,gif}',
-    '**/**/*.css'
-  ];
-
-  browserSync.init(files, {
+function watch() {
+  browserSync.init( {
     // Read here http://www.browsersync.io/docs/options/
     proxy: dev_url,
     port: 8888,
     injectChanges: true
   });
-});
+  gulp.watch('**/*.scss', style)
+  gulp.watch('**/*/*.php').on('change',browserSync.reload);
+  gulp.watch('js/**/*.js').on('change', browserSync.reload);
+}
+exports.style = style;
+exports.watch = watch;
 
 
-/**
- * Compile files from _scss into css (for live injecting)
- */
-// gulp.task('sass', function() {
-//   return gulp.src('_sass/main.scss')
-//     .pipe(sass({
-//       onError: browserSync.notify,
-//       outputStyle: 'compressed'
-//     }))
-//     .pipe(gulp.dest('stylesheets'))
-//     .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-//     .pipe(browserSync.reload({stream:true}))
-//     .pipe(gulp.dest('stylesheets'));
+
+
+
+
+
+
+
+
+
+
+// // const cp = require('child_process');
+// // const sourcemaps = require('gulp-sourcemaps');
+
+// // https://www.npmjs.com/package/gulp-sass
+// const sass = require('gulp-sass')(require('sass'));
+
+// const server = browserSync.create();
+// const dev_url = "localhost:8888/jobs2"
+
+// // sass.compiler = require('dart-sass');
+
+// // BrowserSync
+// function browser_sync(done) {
+//   browserSync.init(files, {
+//     server: {
+//       proxy: dev_url,
+//       port: 8888,
+//       injectChanges: true
+//     }
+//   });
+//   done();
+// }
+
+// function browserSyncReload(done) {
+//   browserSync.reload();
+//   done();
+// }
+
+// /**
+//  * Browser sync for WP theme
+//  */
+// gulp.task('browser-sync',  function() {
+//   // var files = [
+//   //   '**/*.php',
+//   //   '**/*.{png,jpg,gif}',
+//   //   '**/**/*.css'
+//   // ];
+
+//   browserSync.init(files, {
+//     // Read here http://www.browsersync.io/docs/options/
+//     proxy: dev_url,
+//     port: 8888,
+//     injectChanges: true
+//   });
 // });
-gulp.task('sass', function() {
-  return gulp.src('./_sass/main.scss')
-    .pipe(sass({
-      outputStyle: 'compressed',
-    }).on('error', sass.logError))
-    .pipe(prefix({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
-    .pipe(gulp.dest('./stylesheets'))
-    .pipe(browserSync.stream());
-});
 
-gulp.task('watch', function() {
-  gulp.watch('_sass/**.scss', gulp.series('sass'));
-});
+// function style() {
+//   return gulp.src('./_sass/**/*.scss')
+//     .pipe(sass({
+//       // outputStyle: 'compressed',
+//       outputStyle: 'expanded',
+//     }).on('error', sass.logError))
+//     .pipe(gulp.dest('./stylesheets'))
+//     .pipe(browserSync.stream());
+// }
 
-gulp.task('default', gulp.series('browser-sync', 'watch'));
+// function watch() {
+//   browserSync.init({
+//     proxy: dev_url,
+//     port: 8888,
+//     injectChanges: true
+//   });
+
+//   gulp.watch('./_sass/**/*.scss', style);
+//   gulp.watch('**/*.{png,jpg,gifphp}', browserSyncReload);
+
+// }
+
+
+// /**
+//  * Compile files from _scss into css (for live injecting)
+//  */
+// // gulp.task('sass', function() {
+// //   return gulp.src('./_sass/**/*.scss')
+// //     .pipe(sass({
+// //       // outputStyle: 'compressed',
+// //       outputStyle: 'expanded',
+// //     }).on('error', sass.logError))
+// //     .pipe(gulp.dest('./stylesheets'))
+// //     .pipe(browserSync.stream());
+// // });
+
+// // gulp.task('watch', function() {
+// //   // gulp.watch('_sass/**/*.scss', gulp.series('sass'));
+// //   gulp.watch('_sass/**/*.scss', sass);
+// //   gulp.watch('**/*.php', browserSync.reload);
+// //   gulp.watch('**/*.js').on('change', browserSync.reload);
+// // });
+
+// // gulp.task('default', gulp.series('browser-sync', 'watch'));
+
+// exports.style = style;
+// exports.watch = watch;
