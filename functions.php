c0a10cc5c752927b776363b9128f12bb77852464
Wayne Graham <wgraham@clir.org>
2023-11-29 10:54:45 -0500

    Lint updates

--- functions.php
+++ functions.php
@@ -1,27 +1,27 @@
 <?php
 
 /**
-* Jobeleon DLF functions and definitions.
-*
-* When using a child theme you can override certain functions (those wrapped
-* in a function_exists() call) by defining them first in your child theme's
-* functions.php file. The child theme's functions.php file is included before
-* the parent theme's file, so the child theme functions would be used.
-*
-* @link  https://codex.wordpress.org/Theme_Development
-* @link  https://codex.wordpress.org/Child_Themes
-*
-* Functions that are not pluggable (not wrapped in function_exists()) are
-* instead attached to a filter or action hook.
-*
-* For more information on hooks, actions, and filters,
-* {@link https://codex.wordpress.org/Plugin_API}
-* @since Jobeleon 1.3
-*/
+ * Jobeleon DLF functions and definitions.
+ *
+ * When using a child theme you can override certain functions (those wrapped
+ * in a function_exists() call) by defining them first in your child theme's
+ * functions.php file. The child theme's functions.php file is included before
+ * the parent theme's file, so the child theme functions would be used.
+ *
+ * @link  https://codex.wordpress.org/Theme_Development
+ * @link  https://codex.wordpress.org/Child_Themes
+ *
+ * Functions that are not pluggable (not wrapped in function_exists()) are
+ * instead attached to a filter or action hook.
+ *
+ * For more information on hooks, actions, and filters,
+ * {@link https://codex.wordpress.org/Plugin_API}
+ * @since Jobeleon 1.3
+ */
 
 /**
-* Add parent style
-*/
+ * Add parent style
+ */
 function theme_enqueue_styles()
 {
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
